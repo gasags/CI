9065dfad381611baca143710657841f5f8df2357
gatien <bastion.gatien@gmail.com>
2019-01-07 16:52:37 +0100

    commit d'init

---
+++ utilisateurs.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 07 Janvier 2019 à 16:49
+-- Version du serveur :  5.6.15-log
+-- Version de PHP :  5.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `music`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `uti_id` int(11) NOT NULL AUTO_INCREMENT,
+  `uti_mdp` varchar(30) NOT NULL,
+  `uti_mail` varchar(30) NOT NULL,
+  `uti_dateInscription` date NOT NULL,
+  `uti_dateLastConnexion` date NOT NULL,
+  `uti_facebook` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`uti_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
