da9871daa1810300863ca45b163be4c909d096a0
DAAS Lassaad <lassaad.daas@gmail.com>
2019-01-28 14:55:38 +0100

    Head

---
+++ includes/head.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- apparemment initial-scale=1 suffit sans width -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">   
+    <link href="includes/style.css" rel="stylesheet">
+    <!-- jQuery -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <!-- JavaScript Boostrap plugin -->
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+
+
