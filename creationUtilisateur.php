dd974e48fd6feddde663d0b8b06761110be186d0
DAAS Lassaad <lassaad.daas@gmail.com>
2019-03-11 10:47:29 +0100

    formulaire création utilisateur

--- creationUtilisateur.php
+++ creationUtilisateur.php
@@ -7,9 +7,9 @@
         <h3>Veuillez saisir vos informations de connexion</h3>
         <form class="form-horizontal" action="connex.php" method="POST">
             <div class="form-group">
-                <label class="control-label col-sm-2" for="mail" >Identifiant :</label>
+                <label class="control-label col-sm-2" for="login" >Identifiant :</label>
                 <div class="inpConnexion">
-                    <input type="text" class="form-control" id="mail" placeholder="Veuillez saisir votre identifiant" name="tb_mail" required>
+                    <input type="text" class="form-control" id="login" placeholder="Veuillez saisir votre identifiant" name="tb_login" required>
                 </div>
             </div>
             <div class="form-group">
@@ -19,14 +19,14 @@
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="pwd">Mot de passe :</label>
+                <label class="control-label col-sm-2" for="pwd2">Mot de passe :</label>
                 <div class="inpConnexion">
-                    <input type="password" class="form-control" id="pwd" placeholder="Veuillez saisir votre mot de passe" name="tb_pwd" required>
+                    <input type="password" class="form-control" id="pwd2" placeholder="Veuillez confirmer votre mot de passe" name="tb_pwd2" required>
                 </div>
             </div>
             <div class="form-group">
                 <div>
-                    <button type="submit" class="btn btn-primary" name='btn_login'>Se connecter</button>
+                    <button type="submit" class="btn btn-primary" name='btn_create'>Créer</button>
                 </div>
             </div>
         </form>
