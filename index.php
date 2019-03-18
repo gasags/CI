2eab8547e02fdd15c084ea84f12a6f8e0ba58423
DAAS Lassaad <lassaad.daas@gmail.com>
2019-03-18 08:53:59 +0100

    page index

--- index.php
+++ index.php
@@ -9,7 +9,51 @@
     }
     ?>
 
-    <div class="container">
-        <h3>Accueil</h3>
-    </div>
+    <body>
+        <div class="container">
+            <h2>Mes favoris</h2>
+            <h3>Par album</h3>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th style="width:5%">Consulter</th>
+                    <th>Nom</th>
+                    <th>Artiste</th>
+                    <th>Année</th>
+                    <th>Prix</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><button type="submit" class="glyphicon glyphicon-search btn btn-info" name="btn_consulter"></button></td>
+                    <td>Black in black</td>
+                    <td>ACDC</td>
+                    <td>1980</td>
+                    <td>50€</td>
+                </tr>
+                </tbody>
+            </table>
+            <h3>Par artiste</h3>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th style="width:5%">Consulter</th>
+                    <th>Nom</th>
+                    <th>Type</th>
+                    <th>Pays</th>
+                    <th>Genre</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><button type="submit" class="glyphicon glyphicon-search btn btn-info" name="btn_consulter"></button></td>
+                    <td>ACDC</td>
+                    <td>hard rock</td>
+                    <td>Australie</td>
+                    <td>hard rock </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
 </html>
